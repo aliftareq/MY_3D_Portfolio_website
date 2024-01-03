443cd86d1934615384b2b6313355b38ccc3f0339
alif <aliftareq@gmail.com>
2024-01-03 14:11:32 +0600

    structures of the landing page intialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
   return (
-  <div> developer portpolio.</div>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+      </div>
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
